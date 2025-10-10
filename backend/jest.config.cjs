11d7c8ed85a48ae3d2604fa8f9eb9c6ba43e6f43
Andres David Valencia <andres.david.valencia@correounivalle.edu.co>
2025-10-10 15:12:18 -0500

    agregar pruebas de integración para registro y login

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,10 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {},
-  testMatch: ['**/tests/**/*.test.js'],
-};
-module.exports = {
-  testEnvironment: 'node',
-  transform: {},
-  testMatch: ['**/tests/**/*.js?(x)']
+  testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.test.jsx'],
+  testPathIgnorePatterns: ['<rootDir>/archived_root/', '<rootDir>/node_modules/'],
 };
